50d6dda3e2295b92fd643c7b66c8f1a72bf5ee24
ThineshBabu KS <85843431+THINESHBABU0910@users.noreply.github.com>
2025-07-10 10:14:21 +0530

    Create Find Divisors Of A Number O(sqrt(n)).py

---
+++ Personal_Problems/Find Divisors Of A Number O(sqrt(n)).py
@@ -0,0 +1,18 @@
+Find Divisors Of A Number O(sqrt(n)).py
+
+from math import *
+def fun(n):
+    div1 = set()
+    for i in range(1, int(sqrt(n))+1):
+        if n % i ==0:
+            div1.add(i)
+            div1.add(n//i)
+            
+    return list(div1)
+
+t= int(input())
+while t:
+    n= int(input())
+    div1 = fun(n)
+    print(*div1)  # need output as list then remove that * else it will give output as 1 2 3  
+t -=1
